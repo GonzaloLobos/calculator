673e8133cc1a51d30cc04947789885cd7f91f772
Gonzalo Lobos <loboscristiang@gmail.com>
2024-03-20 12:58:58 -0300

    Add keyboard support

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-const operationBtns = document.querySelectorAll(".operator");
+const operatorBtn = document.querySelectorAll(".operator");
 const digitBtns = document.querySelectorAll(".digit");
 const display = document.querySelector(".display");
 const equals = document.querySelector(".equals");
@@ -35,48 +35,44 @@ function resetValues() {
   op = "";
 }
 
-digitBtns.forEach((btn) =>
-  btn.addEventListener("click", function () {
-    if (!op) {
-      while (a.length < 16) {
-        a.push(btn.value);
-        updateDiplay(a.join(""));
-        break;
-      }
+function handleDigit(digit) {
+  if (!op) {
+    while (a.length < 16) {
+      a.push(digit);
+      updateDiplay(a.join(""));
+      break;
+    }
+  } else {
+    while (b.length < 16) {
+      b.push(digit);
+      updateDiplay(b.join(""));
+      break;
+    }
+  }
+}
+
+function handleOperator(operator) {
+  if (op && a[0] && b[0]) {
+    const result = operate(op, a, b);
+    if (result == Infinity) {
+      updateDiplay("Math Error");
+      resetValues();
     } else {
-      while (b.length < 16) {
-        b.push(btn.value);
-        updateDiplay(b.join(""));
-        break;
-      }
+      a = [];
+      b = [];
+      a.push(result);
+      op = operator;
     }
-  })
-);
+  } else if (a[0]) {
+    op = operator;
+  }
 
-operationBtns.forEach((btn) =>
-  btn.addEventListener("click", function () {
-    if (op && a[0] && b[0]) {
-      const result = operate(op, a, b);
-      if (result == Infinity) {
-        updateDiplay("Math Error");
-        resetValues();
-      } else {
-        a = [];
-        b = [];
-        a.push(result);
-        op = btn.value;
-      }
-    } else if (a[0]) {
-      op = btn.value;
-    }
+  if (op && a[0]) {
+    decimalBtn.removeAttribute("disabled");
+  }
+}
 
-    if (op && a[0]) {
-      decimalBtn.removeAttribute("disabled");
-    }
-  })
-);
-
-equals.addEventListener("click", function () {
+function handleEquals() {
   if (op && a[0] && b[0]) {
     const result = operate(op, a, b);
     if (result == Infinity) {
@@ -86,14 +82,9 @@ equals.addEventListener("click", function () {
     }
     resetValues();
   }
-});
+}
 
-clear.addEventListener("click", function () {
-  resetValues();
-  updateDiplay("Calculator");
-});
-
-undo.addEventListener("click", function () {
+function handleUndo() {
   if (!op) {
     a.pop();
     a[0] ? updateDiplay(a.join("")) : updateDiplay("0");
@@ -107,9 +98,9 @@ undo.addEventListener("click", function () {
       decimalBtn.removeAttribute("disabled");
     }
   }
-});
+}
 
-decimalBtn.addEventListener("click", function () {
+function handleDecimal() {
   if (!op) {
     a.push(".");
     decimalBtn.setAttribute("disabled", "");
@@ -118,5 +109,57 @@ decimalBtn.addEventListener("click", function () {
     b.push(".");
     decimalBtn.setAttribute("disabled", "");
     updateDiplay(b.join(""));
+  }
+}
+
+digitBtns.forEach((btn) =>
+  btn.addEventListener("click", () => handleDigit(btn.value))
+);
+
+operatorBtn.forEach((btn) =>
+  btn.addEventListener("click", () => handleOperator(btn.value))
+);
+
+equals.addEventListener("click", handleEquals);
+
+undo.addEventListener("click", handleUndo);
+
+decimalBtn.addEventListener("click", handleDecimal);
+
+clear.addEventListener("click", () => {
+  resetValues();
+  updateDiplay("Calculator");
+});
+
+window.addEventListener("keydown", (e) => {
+  let keyPressed = e.key;
+  switch (keyPressed) {
+    case "0":
+    case "1":
+    case "2":
+    case "3":
+    case "4":
+    case "5":
+    case "6":
+    case "7":
+    case "8":
+    case "9":
+      handleDigit(keyPressed);
+      break;
+    case "+":
+    case "-":
+    case "*":
+    case "/":
+      handleOperator(keyPressed);
+      break;
+    case "Enter":
+      handleEquals();
+      break;
+    case "Backspace":
+      handleUndo();
+      break;
+    case ".":
+      handleDecimal();
+      break;
   }
 });
